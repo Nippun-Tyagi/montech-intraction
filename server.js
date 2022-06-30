945d82ce4232d31c4bfc5b97ba9fd3e2065c54e9
RobinSinghal123 <robin.singhal@oodles.io>
2022-06-30 12:29:22 +0530

    Update env file

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const app = require("./app")
 const dotenv = require("dotenv").config({path:"config/config.env"})
 const connectDatabase = require("./config/Database")
 
-const Port = process.env.PORT
+const port = process.env.PORT 
 
 connectDatabase()
-app.listen(Port,()=>{
-    console.log(`Server is running on http://localhost/${Port}`);
+app.listen(port,()=>{
+    console.log(`Server is running on http://localhost/${port}`);
 })
